9cc7729b2ca1b9b74b64f3bdecdcd8304e6691c6
Egor Bogatyrev <runtfint.runtfint@mail.ru>
2023-02-27 02:46:54 +0500

    dynamically loading posts by scroll (OBSERVER)

---
+++ src/hooks/useObserver.js
@@ -0,0 +1,20 @@
+import { useEffect , useRef } from 'react'
+
+export const useObserver = (ref, canLoad, isPostsLoading, callback) => {
+    const observer = useRef()
+
+    useEffect(() => {
+		if(isPostsLoading) return
+		if(observer.current) observer.current.disconnect()
+
+		var cb = (entries, observer) => {
+			if(entries[0].isIntersecting && canLoad){
+				callback()
+			}
+		}
+
+		observer.current = new IntersectionObserver(cb)
+		observer.current.observe(ref.current)
+
+	}, [isPostsLoading])
+}
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import PostService from '../API/PostService';
-import PostFilter from '../components/PostFilter';
-import PostForm from '../components/PostForm';
-import PostList from '../components/PostList';
-import MyButton from '../components/UI/button/MyButton';
-import MyLoader from '../components/UI/loader/MyLoader';
-import MyModal from '../components/UI/modal/MyModal';
-import MyPagination from '../components/UI/pagination/MyPagination';
-import { useFetching } from '../hooks/useFetching';
-import { usePosts } from '../hooks/usePosts';
+import React, { useEffect, useState } from 'react'
+import { useRef } from 'react'
+import PostService from '../API/PostService'
+import PostFilter from '../components/PostFilter'
+import PostForm from '../components/PostForm'
+import PostList from '../components/PostList'
+import MyButton from '../components/UI/button/MyButton'
+import MyLoader from '../components/UI/loader/MyLoader'
+import MyModal from '../components/UI/modal/MyModal'
+import MyPagination from '../components/UI/pagination/MyPagination'
+import MySelect from '../components/UI/select/MySelect'
+import { useFetching } from '../hooks/useFetching'
+import { useObserver } from '../hooks/useObserver'
+import { usePosts } from '../hooks/usePosts'
 import '../styles/App.css'
-import { getPagesCount } from '../utils/pages';
+import { getPagesCount } from '../utils/pages'
 
 function Posts() {
 	const [posts, setPosts] = useState([]);
@@ -25,11 +28,13 @@ function Posts() {
 
 	const [page, setPage] = useState(1);
 
+	const lastElement = useRef()
+
 	
 
 	const [fetchPosts, isPostsLoading, postError] = useFetching( async (limit, page) => {
 		const response = await PostService.getAll(limit, page)
-		setPosts(response.data)
+		setPosts([...posts, ...response.data])
 		const totalCount = response.headers['x-total-count']
 		setTotalPages(getPagesCount(totalCount, limit))
 	})
@@ -45,7 +50,6 @@ function Posts() {
 
 	const changePage = (page) => {
 		setPage(page)
-		fetchPosts(limit, page)
 	}
 
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
@@ -59,7 +63,11 @@ function Posts() {
 */
 	useEffect(() => {
 		fetchPosts(limit, page)
-	}, []);
+	}, [page, limit])
+
+	useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+		setPage(page + 1)
+	})
 
 	return (
 		<div className="App">
@@ -78,16 +86,29 @@ function Posts() {
 				filter = {filter}
 				setFilter = {setFilter}
 			/>
-			{postError &&
+
+			<MySelect
+				value={limit}
+				onChange={ value=> setLimit(value) }
+				defaultValue="Кол-во элементов на странице"
+				options={[
+					{value: 5, name: "5"},
+					{value: 10, name: "10"},
+					{value: 25, name: "25"},
+					{value: -1, name: "Показать все"},
+				]}
+			/>
+			{ postError &&
 				<h1>Произошла ошибка {postError}</h1>
 			}
-			{isPostsLoading
-				? <div style={{ display:'flex', justifyContent:'center', marginTop:30 }}><MyLoader/></div>
-				: <PostList
-					posts={ sortedAndSearchedPosts }
-					remove = { removePost }
-					title = "JS Posts"
-				  />
+			<PostList
+				posts={ sortedAndSearchedPosts }
+				remove = { removePost }
+				title = "JS Posts"
+			/>
+			<div ref={lastElement} style={{ height:20 }}/>
+			{ isPostsLoading &&
+				<div style={{ display:'flex', justifyContent:'center', marginTop:30 }}><MyLoader/></div>
 			}
 			<MyPagination
 				changePage = {changePage}
